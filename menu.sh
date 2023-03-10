6d90f1fece4dd5f3ae6c2f41585764f3c4337ae7
Raneen AK <raneen.ak@gmail.com>
2023-03-10 11:43:12 +0200

    This is an updated file with the calling of the functions

--- menu.sh
+++ menu.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+source Add_Record.sh
+source Delete_Record.sh
+source Search.sh 
+source Update_Name.sh
+source Update_Amount.sh
+source Print_Amount.sh
+source Print_Sorted.sh
+
 echo "Please select your desired option"
 options=("Add_Record" "Delete Record" "Search" "Update Name" "Update Amount" "Print Amount" "Print Sorted" "Exit")
 COLUMNS=1
