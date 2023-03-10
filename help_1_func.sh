d48ecb4ad3d88e46ad0f3aff28f1b230eca836af
Semion Tch <94633514+SemionChi@users.noreply.github.com>
2023-03-10 13:52:50 +0200

    Update help_1_func.sh

--- help_1_func.sh
+++ help_1_func.sh
@@ -2,7 +2,7 @@
 function get_menu(){
 
 file_search_name=$1
-options=( $( cat "reshooma.txt" | awk -F"," '{print $1}' | grep "$file_search_name") )
+options=( $( cat "recordFileName.txt" | awk -F"," '{print $1}' | grep "$file_search_name") )
 select opt in "${options[@]}" "Quit" ; do
   if (( REPLY == 1 + "${#options[@]}" )) ; then
     exit
