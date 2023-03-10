2841f62345cf644e298f5ac431c0f620490f32eb
Semion <semionchi@gmail.com>
2023-03-10 14:41:32 +0200

    Added Log function

--- Search.sh
+++ Search.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+source make_log.sh
 function Search(){ 
 read -p "Enter record name to serach in DB: " record_name
-echo $(cat "recordFileName.txt" | awk -F"," '{print $1}' | grep -i "$record_name" | sort)
+echo $(cat "recordFileName.txt" | awk -F"," '{print $1}' | grep "$record_name" | sort)
+make_log ${FUNCNAME[0]} 0 #0 for Success
 }
+
