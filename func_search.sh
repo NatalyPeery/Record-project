498bbb397f296b84f32e92bce87de70813fadf6f
Semion Tch <94633514+SemionChi@users.noreply.github.com>
2023-03-10 13:52:30 +0200

    Update func_search.sh

--- func_search.sh
+++ func_search.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 read -p "Enter record name to serach in DB: " record_name
-echo $(cat "reshooma.txt" | awk -F"," '{print $1}' | grep -i "$record_name" | sort)
+echo $(cat "recordFileName.txt" | awk -F"," '{print $1}' | grep -i "$record_name" | sort)
