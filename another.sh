c5d31ab507d3fda89cd6f0a621ff4d89f7a8ede0
Semion <semionchi@gmail.com>
2023-03-10 13:50:23 +0200

    Added search func and small fixes to help1 func

--- another.sh
+++ another.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-#!/bin/bash
-file_search_name=$1
-options=( $( cat "reshooma.txt" | awk -F"," '{print $1}' | grep "$file_search_name") )
-PS3="$prompt "
-select opt in "${options[@]}" "Quit" ; do
-  if (( REPLY == 1 + "${#options[@]}" )) ; then
-    exit
+source help_1_func.sh
 
-  elif (( REPLY > 0 && REPLY <= "${#options[@]}" )) ; then
-    echo  "You picked $opt"
-    break
+read -p "Search for a record: " rec_name
 
-  else
-    echo "Invalid option. Try another one."
-  fi
-done
-
+get_menu $rec_name
