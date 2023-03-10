05f5a3a584b044e3d761ff24884edeabf864120c
Semion Tch <94633514+SemionChi@users.noreply.github.com>
2023-03-10 13:59:22 +0200

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-source Add_Record.sh
-source Delete_Record.sh
+#source Add_Record.sh
+#source Delete_Record.sh
 source Search.sh 
-source Update_Name.sh
-source Update_Amount.sh
-source Print_Amount.sh
-source Print_Sorted.sh
+#source Update_Name.sh
+#source Update_Amount.sh
+#source Print_Amount.sh
+#source Print_Sorted.sh
 
 echo "Please select your desired option"
 options=("Add_Record" "Delete Record" "Search" "Update Name" "Update Amount" "Print Amount" "Print Sorted" "Exit")
