a7af3a4e70afa2f3494f8f4a2523232f31dfad60
Semion Tch <94633514+SemionChi@users.noreply.github.com>
2023-03-10 14:00:10 +0200

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -13,13 +13,13 @@ options=("Add_Record" "Delete Record" "Search" "Update Name" "Update Amount" "Pr
 COLUMNS=1
 select i in "${options[@]}" ; do
 case $i in
-"Add_Record" Add_Record ;;
-"Delete Record" Delete_Record ;;
+"Add_Record" ;;
+"Delete Record"  ;;
 "Search" Search ;;
-"Update Name" Update_Name ;;
-"Update Amount" Update_Amount ;;
-"Print Amount" Print_Amount
-"Print Sorted" Print_Sorted ;;
+"Update Name"  ;;
+"Update Amount"  ;;
+"Print Amount" ;;
+"Print Sorted"  ;;
 "Exit") exit ;;
 *) echo "Invalid option please try again" ;;
 esac
