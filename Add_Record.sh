d5fbd185d4d9991da0b2b37b28844f690f3502be
Moshe <hvtcob@gmail.com>
2023-03-12 18:17:29 +0200

    final update functions

--- Add_Record.sh
+++ Add_Record.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+source make_log.sh
 source if_exists.sh
 source help_1_func.sh
 source Search.sh
@@ -13,8 +14,10 @@ then
     then
         echo $record,$amount >> recordFileName.txt
         echo "new record enterd"
+        make_log ${FUNCNAME[0]} 0
     else
         echo "you allready have this record"
+        make_log ${FUNCNAME[0]} 1
         echo "If none of the selections matches your request please choose Quit to add to the directory"
         selection=$(get_menu $record) 
         if [[ $selection == "exit" ]]
@@ -24,9 +27,11 @@ then
         number=$( cat "recordFileName.txt" | grep -w "$selection" | awk -F"," '{print $2}' | sort --unique )
         let amount=$number+$amount
         sed -i "s/^$selection,.*/$selection,$amount/g" recordFileName.txt
+        make_log ${FUNCNAME[0]} 0
     fi
 else 
     echo "enter the parameters"
+    make_log ${FUNCNAME[0]} 1
 fi
 }
 Add_Record
