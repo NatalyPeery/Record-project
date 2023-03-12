00d04d570e2ce1e4c5914393a034622484644676
Raneen AK <raneen.ak@gmail.com>
2023-03-12 09:22:37 +0200

    This is an updated file

--- menu.sh
+++ menu.sh
@@ -13,13 +13,13 @@ options=("Add_Record" "Delete Record" "Search" "Update Name" "Update Amount" "Pr
 COLUMNS=1
 select i in "${options[@]}" ; do
 case $i in
-"Add_Record" Add_Record ;;
-"Delete Record" Delete_Record ;;
-"Search" Search ;;
-"Update Name" Update_Name ;;
-"Update Amount" Update_Amount ;;
-"Print Amount" Print_Amount
-"Print Sorted" Print_Sorted ;;
+"Add_Record") Add_Record ;;
+"Delete Record") Delete_Record ;;
+"Search") Search ;;
+"Update Name") Update_Name ;;
+"Update Amount") Update_Amount ;;
+"Print Amount") Print_Amount
+"Print Sorted") Print_Sorted ;;
 "Exit") exit ;;
 *) echo "Invalid option please try again" ;;
 esac
